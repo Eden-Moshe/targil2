3683424a8450d04120962626a294ef72de9b4afb
Eden-Moshe <edenmoshe160299@gmail.com>
2024-11-19 12:56:51 +0200

    added documentation in SharedData

--- Targil2Shitot/src/SharedData.java
+++ Targil2Shitot/src/SharedData.java
@@ -1,18 +1,47 @@
 import java.util.ArrayList;
 
+
+/** Description of ShareData
+ * @author Eden Moshe
+ * @author Shani Fahima
+ */
 public class SharedData 
 {
+	/** Description of array
+	 * creating an empty list
+	 */
 	private ArrayList<Integer> array; // Changed from int[] to ArrayList<Integer>
+	
+	/** Description of winArray
+	 *  creating an array of boolean values
+	 */
+	
 	private boolean [] winArray;
+	
+	/** Description of flag
+	 *  creating a boolean value
+	 */
 	private boolean flag;
+	
+	/** Description of b
+	 *  creating a value for list size
+	 */
+
 	private final int b;
 	
+	/** Description of SharedData()
+	 * @param list of Integer called "array"
+	 * @param b is lists' size
+	 */
 	public SharedData(ArrayList<Integer> array, int b) {
 		
 		this.array = array;
 		this.b = b;
 	}
 
+	/**Description of getWinArray()
+	 * @return is array of boolean values
+	 */
 	public boolean[] getWinArray() 
 	{
 		return winArray;
